5182592b68f097c6d669aee3f236afe2dc846be6
AndreasvonU <andreasvonu@gmail.com>
2020-12-25 10:25:38 +0100

    First draft

--- filmify/src/requests.js
+++ filmify/src/requests.js
@@ -3,6 +3,7 @@ const API_KEY = "4b780ecaac3c30fefa5995543355bb44";
 const requests = {
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  searchMovie: `/https://api.themoviedb.org/3/search/company?api_key=${API_KEY}&query=joker`,
 };
 
 export default requests;
